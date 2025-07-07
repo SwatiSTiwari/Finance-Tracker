08a1d922c32f5874c1d454ee7d7f34a9c854ab49
swati <swatistiwati13@gmail.com>
2025-07-07 14:29:56 +0530

    fixed the type issue

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  // Disable trace files that cause permission issues on OneDrive
+  experimental: {
+    instrumentationHook: false,
+  },
+  // Disable webpack cache to avoid permission issues
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
